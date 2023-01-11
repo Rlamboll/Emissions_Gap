89123b3e882d52ba9b01d37e129c450a0571a489
RLamboll <rlamboll@hotmail.co.uk>
2023-01-11 18:17:40 +0000

    Initial commit

---
+++ scripts/calc_temp_quants.py
@@ -0,0 +1,29 @@
+import os
+import pandas as pd
+
+version = "v22.4"
+outfile = f"../output/{version}/2022_emission_gap_temp_summary_data.csv"
+fairdir = f"../output/{version}/fair_temperatures/"
+scenariofiles = [
+        x for x in os.listdir(fairdir)
+        if x.endswith('.csv')
+]
+temp_quants = [0.1, 0.167, 0.2, 0.3, 0.33, 0.4, 0.5, 0.6, 0.66, 0.7, 0.8, 0.833, 0.9]
+results = []
+for file in scenariofiles:
+    tmp = pd.read_csv(fairdir + file, index_col="year")
+    for q in temp_quants:
+        quant_res = tmp.quantile(q, axis=1)
+        quant_res = pd.DataFrame(quant_res).T
+        quant_res["quantile"] = q
+        if file.split("_-_")[0] != "NDC_case":
+            quant_res["scenario"] = file.split("_-_")[1]
+            quant_res["model"] = file.split("_-_")[0]
+        else:
+            quant_res["scenario"] = file.split("_-_")[2]
+            quant_res["model"] = file.split("_-_")[0] + "_-_" + file.split("_-_")[1]
+        results.append(quant_res)
+
+results = pd.concat(results)
+results = results.loc[:, ["model", "scenario", "quantile"] + list(results.columns[:-3])]
+results.to_csv(outfile, index=False)
---
+++ scripts/run_fair.py
@@ -0,0 +1,140 @@
+import json
+from multiprocessing import Pool, cpu_count, freeze_support
+
+from climateforcing.utils import mkdir_p
+import fair
+from fair.tools.magicc import scen_open
+import os
+import numpy as np
+import pandas as pd
+from tqdm import tqdm
+
+with open('../input/fair-1.6.2-ar6/fair-1.6.2-wg3-params.json') as f:
+    config_list = json.load(f)
+natural_ems = pd.read_csv(
+    "../input/fair-1.6.2-ar6/fair_wg3_natural_ch4_n2o.csv", sep=","
+)
+
+emissions_in = {}
+results_out = {}
+WORKERS = cpu_count() - 1
+version = "v22.4"
+outdir = '../output/{}/fair_{}/'
+parallel_processing = True
+end_year = 2100
+start_year = natural_ems.loc[0, "year"]
+inter_start_year = 1850   # First year we actually care about values from
+natural_ems = natural_ems.iloc[:end_year-start_year+1, 1:].values
+
+scen_file_dir = f"../output/scens/{version}/"
+scens_to_run = [
+    x for x in os.listdir(scen_file_dir)
+    if x.endswith('.SCEN')
+]
+
+# Often want to restrict number of runs in one go
+scens_to_run = scens_to_run[:]
+
+scenarios = [x[:-5] for x in scens_to_run]
+check_prehist = np.loadtxt(
+    '../input/fair-1.6.2-ar6/ssp245_constant-2020-ch4.csv',
+    delimiter=','
+)
+for i, scenario in enumerate(scenarios):
+    tmp = scen_open(scen_file_dir + scens_to_run[i])
+    if tmp[0, 0] != start_year:
+        if check_prehist[0, 0] == start_year:
+            needed_rows = int(tmp[0, 0] - start_year)
+            tmp = np.concatenate([check_prehist[:needed_rows, :], tmp])
+
+    emissions_in[scenario] = tmp
+
+def run_fair(args):
+    thisC, thisF, thisT, _, thisOHU, _, thisAF = fair.forward.fair_scm(**args)
+    return (thisC[:, 0], thisC[:, 1], thisT, thisF[:, 1], np.sum(thisF, axis=1))
+
+
+def fair_process(emissions):
+    updated_config = []
+    for i, cfg in enumerate(config_list):
+        updated_config.append({})
+        for key, value in cfg.items():
+            if isinstance(value, list):
+                updated_config[i][key] = np.asarray(value)
+            else:
+                updated_config[i][key] = value
+        emissions_length = len(emissions)
+        project_length = len(updated_config[i]["F_solar"])
+        updated_config[i]['emissions'] = emissions
+        updated_config[i]['diagnostics'] = 'AR6'
+        updated_config[i]["efficacy"] = np.ones(45)
+        updated_config[i]["gir_carbon_cycle"] = True
+        updated_config[i]["temperature_function"] = "Geoffroy"
+        updated_config[i]["aerosol_forcing"] = "aerocom+ghan2"
+        updated_config[i]["fixPre1850RCP"] = False
+        #    updated_config[i]["scale"][43] = 0.6
+        updated_config[i]["F_solar"][270:] = 0
+        updated_config[i]["natural"] = natural_ems
+        if project_length < emissions_length:
+            updated_config[i]["F_volcanic"] = np.pad(updated_config[i]["F_volcanic"], (0, emissions_length-project_length))
+            updated_config[i]["F_solar"] = np.pad(updated_config[i]["F_solar"], (0, emissions_length-project_length))
+        elif project_length > emissions_length:
+            updated_config[i]["F_volcanic"] = updated_config[i]["F_volcanic"][: emissions_length]
+            updated_config[i]["F_solar"] = updated_config[i]["F_solar"][: emissions_length]
+
+    if __name__ == '__main__':
+        if parallel_processing:
+            with Pool(WORKERS) as pool:
+                result = list(
+                    tqdm(pool.imap(run_fair, updated_config), total=len(updated_config),
+                         position=0, leave=True))
+
+            result_t = np.array(result).transpose(1, 2, 0)
+            c_co2, c_ch4, t, f_ch4, f_tot = result_t
+        else:
+            shape = (end_year - start_year + 1, len(updated_config))
+            c_co2 = np.ones(shape) * np.nan
+            c_ch4 = np.ones(shape) * np.nan
+            t = np.ones(shape) * np.nan
+            f_ch4 = np.ones(shape) * np.nan
+            f_tot = np.ones(shape) * np.nan
+            for i, cfg in tqdm(enumerate(updated_config), total=len(updated_config),
+                               position=0, leave=True):
+                c_co2[:, i], c_ch4[:, i], t[:, i], f_ch4[:, i], f_tot[:, i] = run_fair(
+                    updated_config[i])
+        temp_rebase = t - t[100:151, :].mean(axis=0)
+    else:
+        raise RuntimeError
+
+    return c_co2, c_ch4, temp_rebase, f_ch4, f_tot
+
+def main():
+    freeze_support()
+    for scenario in tqdm(scenarios, position=0, leave=True):
+        results_out[scenario] = {}
+        (
+            results_out[scenario]['co2_concentrations'],
+            results_out[scenario]['ch4_concentrations'],
+            results_out[scenario]['temperatures'],
+            results_out[scenario]['ch4_effective_radiative_forcing'],
+            results_out[scenario]['effective_radiative_forcing']
+        ) = fair_process(emissions_in[scenario])
+
+    for scenario in scenarios:
+        for var in [
+            'co2_concentrations', 'ch4_concentrations', 'temperatures',
+            'ch4_effective_radiative_forcing', 'effective_radiative_forcing',
+        ]:
+            mkdir_p(outdir.format(version, var.replace("effective_radiative_forcing", "erf")))
+            df_out = pd.DataFrame(
+                results_out[scenario][var][
+                inter_start_year - start_year: end_year + 1 - start_year, :]
+            )
+            df_out['year'] = np.arange(inter_start_year, end_year + 1)
+            df_out.set_index('year', inplace=True)
+            df_out.to_csv(
+                outdir.format(version, var.replace("effective_radiative_forcing", "erf")
+            ) + '{}.csv'.format(scenario), float_format="%6.4f")
+
+if __name__ == "__main__":
+    main()
