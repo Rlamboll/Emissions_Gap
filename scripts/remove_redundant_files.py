4de56950e643bae77daaa75a723c5f58784d1036
RLamboll <rlamboll@hotmail.co.uk>
2023-03-20 16:20:40 +0000

    Added remaining runs for robustness checks

--- scripts/remove_redundant_files.py
+++ scripts/remove_redundant_files.py
@@ -1,15 +1,14 @@
 import os
 
-version = "v22.6"
+version = "v22.7"
 
-scen_file_dir = '../output/scens/v22.6/'
+scen_file_dir = f'../output/scens/{version}/'
 scens_to_run = [
     x[:-5] for x in os.listdir(scen_file_dir)
     if x.endswith('.SCEN')
 ]
 print()
 pointless = [s for s in scens_to_run if "nz_CO2_only_cert_higher" in s]
-
 print(pointless)
 for file in pointless:
     try:
