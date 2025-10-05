0004f9fa127e5a11903093c39f598db2acaa96b2
Lamboll <rlamboll@ic.ac.uk>
2025-10-05 23:07:09 +0100

    Intermediate version sketching out results for 2025.

--- scripts/calc_temp_quants.py
+++ scripts/calc_temp_quants.py
@@ -1,8 +1,8 @@
 import os
 import pandas as pd
 
-version = "v24.2"
-outfile = f"../output/{version}/2024_emission_gap_temp_summary_data.csv"
+version = "v25.1"
+outfile = f"../output/{version}/emission_gap_temp_summary_data.csv"
 fairdir = f"../output/{version}/fair_temperatures/"
 scenariofiles = [
         x for x in os.listdir(fairdir)
